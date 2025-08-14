fc39f31478fffa94fbf77eb353d25267c9787930
foisal404 <foisalahmmed2@gmail.com>
2025-08-14 14:36:23 +0600

    feat: mongodb connection add

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,9 +5,11 @@ dotenv.config();
 interface Config {
   port: number;
   nodeEnv: string;
+  mongoURI: string;
 }
 
 const config: Config = {
+  mongoURI: process.env.MONGO_URI || "",
   port: Number(process.env.PORT) || 5000,
   nodeEnv: process.env.NODE_ENV || "development",
 };
