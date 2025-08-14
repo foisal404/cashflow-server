94f8a3a63c1bbcc27c163b124bac53e88e4b1b55
foisal404 <foisalahmmed2@gmail.com>
2025-08-14 16:47:08 +0600

    feat: routes add in main file

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
 import express from "express";
+import cors from "cors";
 import { errorHandler } from "./middlewares/errorHandler";
+import authRoutes from "./routes/auth.routes";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 // Routes
 app.get("/", (req, res) => {
   res.send("CashFlow API is running!");
 });
+app.use("/api/auth", authRoutes);
 
 // Global error handler (should be after routes)
 app.use(errorHandler);
