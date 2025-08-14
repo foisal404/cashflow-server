84799c1fdc97911344d686bdd81ac156b008fa8d
foisal404 <foisalahmmed2@gmail.com>
2025-08-14 14:23:52 +0600

    create: initial express typescript server setup

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import { errorHandler } from "./middlewares/errorHandler";
+
+const app = express();
+
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("CashFlow API is running!");
+});
+
+// Global error handler (should be after routes)
+app.use(errorHandler);
+
+export default app;
