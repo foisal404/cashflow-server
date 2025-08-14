2d0c337ec989ca199ba1d2008d9d52dc41f0e1fe
foisal404 <foisalahmmed2@gmail.com>
2025-08-14 16:30:05 +0600

    feat: create user model

---
+++ src/models/User.model.ts
@@ -0,0 +1,15 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const userSchema = new Schema<IUser>({
+  name: { type: String, required: true, minlength: 3 },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, minlength: 6 },
+});
+
+export default mongoose.model<IUser>("User", userSchema);
