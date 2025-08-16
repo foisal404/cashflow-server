b0da6424453e9e87a03af732a5b038b2a5dd96e6
foisal404 <foisalahmmed2@gmail.com>
2025-08-16 22:59:24 +0600

    logout routes add

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,11 +1,17 @@
 import { Router } from "express";
-import { register, login, getProfile } from "../controller/auth.controller";
+import {
+  register,
+  login,
+  getProfile,
+  logout,
+} from "../controller/auth.controller";
 import { isAuthenticated } from "../middlewares/auth.middleware";
 
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/logout", isAuthenticated, logout);
 router.get("/profile", isAuthenticated, getProfile);
 
 export default router;
